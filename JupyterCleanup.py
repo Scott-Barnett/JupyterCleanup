f708fc20b5ca6f1f676864dfde9c198944cdfedd
Scott Barnett <sacbarnett@gmail.com>
2020-12-04 15:38:04 +0000

    Add Jupyter Cleanup

---
+++ JupyterCleanup.py
@@ -0,0 +1,51 @@
+import json
+import copy
+import sys
+
+class CleanNotebook:
+    """
+    Takes a Jupyter Notebook (.ipynb) file and strips out all the environment specific info (python version, output etc)
+
+    @param nb_path: Path to the .ipynb fileS
+    """
+    def __init__(self, nb_path: str):
+        self.nb_path = nb_path
+        self.data = self.open_nb()
+        if self.data is None:
+            print("Notebook does not exist")
+            return
+        
+        new_nb = self.cleanup_code()
+        self.write_nb(new_nb)
+
+    def cleanup_code(self) -> dict:
+        """
+        Takes the .ipynb data (in the json form) and cleans the notebook metadata, and removes any output
+        """
+        new_data = copy.deepcopy(self.data)
+        new_data["metadata"]["language_info"]["version"] = 3
+        new_data["metadata"].pop('kernelspec')
+        for index, cell in enumerate(self.data["cells"]):
+            if cell["cell_type"] == "code":
+                new_data["cells"][index]["metadata"] = {}
+                new_data["cells"][index]["outputs"] = []
+                new_data["cells"][index]["execution_count"] = None
+        return new_data
+
+    def open_nb(self) -> dict:
+        try:
+            with open(self.nb_path, 'r') as nb:
+                data = json.load(nb)
+            return data
+        except FileNotFoundError:
+            return None
+
+    def write_nb(self, data: dict) -> bool:
+        try:
+            with open(self.nb_path, 'w') as nb:
+                json.dump(data, nb, indent=1)
+            return True
+        except FileNotFoundError:
+            return False
+
+CleanNotebook(sys.argv[1])
---
+++ README.md
@@ -0,0 +1,8 @@
+# Jupyter CleanUp
+
+This program takes a .ipynb file and removes any excess data that could cause issues if accidentally committed to git.
+
+## Usage
+```
+python JupyterCleanup.py <path_to_ipynb>
+```
